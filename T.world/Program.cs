d76d68d3c52f1b259a2cfed18c5253b4c9af06a0
Bao <thienbao99@gmail.com>
2025-04-19 14:49:13 +0700

    design login and resigter

--- T.world/Program.cs
+++ T.world/Program.cs
@@ -16,7 +16,7 @@ namespace T.world
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Home());
         }
     }
 }
